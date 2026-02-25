5597f7f968599341e791e3f39f615cbfd852a7c0
jyoshitaa <jyoshitaa9806@gmail.com>
2026-02-25 16:11:30 +0530

    UC6

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        boolean result = true;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string to check if it is a palindrome:");
-        String word = scanner.nextLine();
+        System.out.println("Enter the String: ");
+        String real = input.nextLine();
 
-        System.out.println("Input text: " + word);
+        Queue q = new LinkedList();
+        Stack st = new Stack();
 
-        if (isPalindrome(word)) {
-            System.out.println("Is it a Palindrome? : true");
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("Is it a Palindrome? : false");
-            System.out.println("The given string is NOT a Palindrome.");
+
+        for(int i=0; i<=real.length()-1; i++) {
+            q.add(real.charAt(i));
+            st.push(real.charAt(i));
         }
 
-        scanner.close();
-    }
-
-    public static boolean isPalindrome(String s) {
-        char[] string = s.toCharArray();
-        int low = 0, high = string.length - 1;
-        while (low <= high) {
-            if (string[low] != string[high]) {
-                return false;
+        while(!st.isEmpty()) {
+            if (q.remove() != st.pop()) {
+                result = false;
             }
-            low++;
-            high--;
         }
-        return true;
-    }
-    public static boolean isPalindromeStack(String s){
-        boolean result=true;
-        Stack<Character> charStack=new Stack<>();
-        char[] charArray = s.toCharArray();
-        int i=0;
-        for(char c : charArray){
-            charStack.push(c);
-        }
-        while(i<charArray.length){
-            if(charStack.pop()!= charArray[i]){
-                result=false;
-            }
-            i++;
-        }
-        return result;
+        System.out.println(real + " is a palindrome? " + result);
     }
 }
